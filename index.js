8d8bae6f23aa09dab1fe2b8a171c075aeb12a0b2
Naveen-beginner <rakurthikowshik@gmail.com>
2026-05-08 17:40:08 +0530

    Production ready

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export { default as emailValidator } from "./validators/email_validator.js"
 
-export { default as passwordValidator } from "./validators/passwordValidator.js"
+export { default as passwordValidator } from "./validators/password_validator.js"
 
 export {
     utilities,
